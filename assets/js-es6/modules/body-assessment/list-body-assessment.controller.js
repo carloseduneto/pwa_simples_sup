f46421aad7fa7f5ec00d03eb1cc6de6481016992
Carlos Eduardo Neto <112590775+carloseduneto@users.noreply.github.com>
2026-04-24 10:44:55 -0300

    adiciona cálculos de percentual de gordura usando Pollock 7 dobras e idade dinâmica

--- assets/js-es6/modules/body-assessment/list-body-assessment.controller.js
+++ assets/js-es6/modules/body-assessment/list-body-assessment.controller.js
@@ -1,7 +1,17 @@
 import { GlobalLoader } from "../../ui/global-loader.js";
+
+// Função auxiliar para calcular idade a partir da data de nascimento e data de registro
 import { BodyAvaliacoesService } from "../body-assessment/body-avaliacoes.service.js";
+
+// User data
+import { AuthService } from "../../services/auth.service.js";
+import { UserContextService } from "../../services/user-context.service.js";
+
+// Função auxiliar para calcular idade a partir da data de nascimento e data de registro
 import { gerarListItem } from "../../components/list-item.js"; // <-- Importa o componente
 import { calcularFaulkner } from "../../utilities/calcularPercentualGordura.js";
+import { calcularPollock7Dobras } from "../../utilities/calcularPercentualGordura.js";
+import { calcularIdade } from "../../utilities/calcularIdade.js";
 
 export async function initBodyAssessmentList(onNavigate) {
   const container = document.getElementById("body-assessment-container");
@@ -17,6 +27,19 @@ export async function initBodyAssessmentList(onNavigate) {
         '<p style="text-align:center; opacity: 0.6;">Nenhuma avaliação corporal encontrada.</p>';
       return;
     }
+
+    const userId = await AuthService.getUserId();
+    if (!userId) {
+      container.innerHTML = "";
+      return;
+    }
+
+    // Busca opções e seleção atual em paralelo (mais rápido)
+    const userData = await Promise.all([
+      UserContextService.getUserData(userId),
+    ]);
+
+    console.log("Dados do usuário para contexto:", userData);
 
     let htmlLista = "";
 
@@ -40,11 +63,55 @@ export async function initBodyAssessmentList(onNavigate) {
       // Se valor existir, cria a string. Se for null/undefined, fica vazio "".
       const percentualGordura = valor ? ` - ${valor}` : "";
 
+      // Para o cálculo do Pollock 7 Dobras, precisamos de mais dobras e informações
+      const idadeDynamic = calcularIdade(
+        userData[0].data_nascimento,
+        avaliacao.data_registro,
+      );
+      console.log("Idade calculada:", idadeDynamic);
+
+      const dobrasPollock = {
+        dobra_tricipital: avaliacao.value.dobra_tricipital,
+        dobra_subescapular: avaliacao.value.dobra_subescapular,
+        dobra_suprailiaca: avaliacao.value.dobra_suprailiaca,
+        dobra_abdominal: avaliacao.value.dobra_abdominal,
+        dobra_peitoral: avaliacao.value.dobra_peitoral,
+        dobra_axilar_media: avaliacao.value.dobra_axilar_media,
+        dobra_coxa: avaliacao.value.dobra_coxa,
+      };
+
+      const idade = 25;
+      const sexo = "masculino";
+
+      const percentualPollock7Dobras = calcularPollock7Dobras(
+        dobrasPollock,
+        idade,
+        sexo,
+      );
+
+      let percentualPollock7DobrasIdade = calcularPollock7Dobras(
+        dobrasPollock,
+        idadeDynamic,
+        sexo,
+      );
+
+      console.log(
+        "Percentual de Gordura (Pollock 7 Dobras):",
+        percentualPollock7Dobras,
+        "Idade dinâmica:",
+        percentualPollock7DobrasIdade,
+      );
+
+      percentualPollock7DobrasIdade = avaliacao.value.gordura == percentualPollock7DobrasIdade || percentualPollock7DobrasIdade == null ? 
+      avaliacao.value.gordura
+      :
+      percentualPollock7DobrasIdade;
+
       htmlLista += gerarListItem({
         id: avaliacao.id,
         // Aqui usamos o index + 1 para começar a contagem do 1
         titulo: `Avaliação #${avaliacoes.length - index}`,
-        subtitulo: `${avaliacao.value.peso} kg, ${avaliacao.value.gordura}${percentualGordura}%`,
+        subtitulo: `${avaliacao.value.peso} kg, ${percentualPollock7DobrasIdade}${percentualGordura}%`,
         titleComplement: `${avaliacao.avaliador}`,
         data: dateItemValue,
         mostrarAcoes: true,
@@ -64,7 +131,7 @@ export async function initBodyAssessmentList(onNavigate) {
       // 2. Extrai o ID uma única vez
       const id = itemContainer.dataset.id;
 
-      if(id){
+      if (id) {
         console.log("Ir para detalhes da avaliação:", id);
         localStorage.setItem("detailBodyAssessmentId", id);
         if (onNavigate) onNavigate("bodyAssessmentDetail");
@@ -117,7 +184,7 @@ export async function initBodyAssessmentList(onNavigate) {
           }
         }
       }
-    };;;;
+    };
   } catch (error) {
     console.error("Falha ao buscar avaliações:", error);
     container.innerHTML =
